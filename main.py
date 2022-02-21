4a53a28fc77c5c4d6df7ada1a37ff044b70aba51
horan <46918417+horanDEV@users.noreply.github.com>
2022-02-21 21:09:16 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-import subprocess
+from subprocess import call, PIPE
+from threading import Thread
+from time import sleep
 
-subprocess.call('python py/update.py', shell=True)
-subprocess.call('python py/server.py', shell=True)
+def flaskserver():
+    call('python py/update.py', shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+    sleep(2)
+    call('python py/server.py', shell=True)
+
+def nodeserver():
+    sleep(2)
+    call('yarn', shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+    call('yarn serve', shell=True)
+
+a = Thread(target=flaskserver)
+b = Thread(target=nodeserver)
+
+def main():    
+    a.start()
+    b.start()
+    b.join()
+    a.join()
+
+if __name__ == "__main__":
+    main()
