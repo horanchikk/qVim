02d72c338529fcd3ebe2e43a5350302dd8c41519
horan <46918417+horanDEV@users.noreply.github.com>
2022-02-22 20:52:54 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,107 @@
+# Exceptions can be: FileNotFoundError(config.json); IndexError(sys.argv); ImportError
+
+try:
+    import json, multiprocessing, requests, psutil
+    from webbrowser import open_new_tab
+    from time import sleep
+    from rich.console import Console
+    from subprocess import call, PIPE
+    from time import sleep
+except:
+    from subprocess import call
+    from sys import exit
+    call('pip3 install rich requests bs4 flask flask-cors lxml psutil', shell=True)
+    call('python main.py', shell=True)
+    exit(0)
+
+
+import json, multiprocessing, requests, psutil
+from webbrowser import open_new_tab
+from time import sleep
+from rich.console import Console
 from subprocess import call, PIPE
-from threading import Thread
 from time import sleep
 
-def flaskserver():
-    call('python py/update.py', shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
-    sleep(2)
-    call('python py/server.py', shell=True)
+server = json.load(open("out/config.json", "r"))
+log = open("out/logme.log", "w")
+console = Console()    
 
-def nodeserver():
-    sleep(2)
-    call('yarn', shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
-    call('yarn serve', shell=True)
+def initThread(typeserver):
+    match typeserver:
+        case "flask":
+            flaskserver = call('python py/server.py', shell=True, stdout=PIPE, stderr=log, stdin=log)
+            print(flaskserver.stderr)
+            while True:
+                pass
 
-a = Thread(target=flaskserver)
-b = Thread(target=nodeserver)
+        case "npm":
+            call('yarn', shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+            npmserver = call("yarn serve", stdout=log, stdin=log, stderr=log, shell=True)
+            print(npmserver.stderr)
+            while True:
+                pass
 
-def main():    
-    a.start()
-    b.start()
-    b.join()
-    a.join()
+g = multiprocessing.Process(target=initThread, args=("npm", ))
+j = multiprocessing.Process(target=initThread, args=("flask", ))
+g.daemon = True
+j.daemon = True
+
+def killall():
+    g.kill()
+    j.kill()
+
+    for proc in psutil.process_iter():
+        if proc.name() == 'node.exe':
+            proc.kill()
+        elif proc.name == 'node':
+            proc.kill()
+
+
+def serverRun():
+    with console.status("[bold yellow] Server starting...\n", spinner="line"):
+        g.start()
+        j.start()
+        try:
+            serverreq = requests.get('http://localhost:5000').status_code
+        except:
+            serverreq = "404"
+
+    if serverreq == 200:
+        serverreq = "[bold green] 200"
+        with console.status(f"[bold cyan] Status code: {serverreq}\n", spinner="line"):
+            sleep(0.5)
+    else:
+        serverreq = "[bold red]" + serverreq
+        with console.status(f"[bold white] Status code: {serverreq}\n", spinner="line"):
+            sleep(2)
+        with console.status(f"[bold white] Try 2...\n", spinner="line"):
+            sleep(2)
+            try:
+                serverreq = requests.get('http://localhost:5000').status_code
+            except:
+                serverreq = "404"
+
+            if serverreq == 200:
+                    sleep(0.5)
+            else:
+                serverreq = "[bold red]" + serverreq
+                sleep(2)
+                raise ConnectionAbortedError('Lost connection to http://localhost:5000')
+
+    open_new_tab('http://localhost:8080')
+
+    with console.status("[bold green] Server has been started! Url: http://localhost:8080\n        For exit use: Ctrl+C\n", spinner="bouncingBar", refresh_per_second=7) :
+        while True:
+            sleep(10000)
 
 if __name__ == "__main__":
-    main()
+    multiprocessing.freeze_support()
+    try:
+        serverRun()
+    except KeyboardInterrupt or SystemExit:
+        killall()
+        console.print('[red bold]qVim server has been stopped')
+    except Exception as e:
+        killall()
+        print("\n" + str(e))
+        console.print('[red bold]qVim server has been stopped')
